933a593178e63bd6efd2774d796d19e162bab8fc
zhang <757337056@qq.com>
2020-07-07 16:31:13 +0800

    gateway

--- gateway/src/main/java/com/sxw/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/sxw/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.sxw.system;
+package com.sxw.gateway;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,13 +10,13 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class SystemApplication {
-    private static final Logger LOG = LoggerFactory.getLogger(SystemApplication.class);
+public class GatewayApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
     public static void main(String[] args){
-        SpringApplication app = new SpringApplication(SystemApplication.class);
+        SpringApplication app = new SpringApplication(GatewayApplication.class);
         Environment env = app.run(args).getEnvironment();
         LOG.info("启动成功");
-        LOG.info("System地址:\thttp://127.0.0.1:{}",env.getProperty("server.port"));
+        LOG.info("Gateway地址:\thttp://127.0.0.1:{}",env.getProperty("server.port"));
 
     }
 
